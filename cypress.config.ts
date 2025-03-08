ab0e95ee4475d1c95517a1c728760dda09984fdd
Omanuegonzalez <est.ogonzalezx0201@uml.edu.ni>
2025-03-08 10:08:18 -0600

    integraccion de cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,8 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: "poj1p4",
   e2e: {
-    baseUrl: 'http://localhost:5174',
+    baseUrl: 'http://localhost:5173',
   },
 })
