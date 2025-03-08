d13e4e22ec7f93e769bde3fc86974495b2711ebb
Omanuegonzalez <est.ogonzalezx0201@uml.edu.ni>
2025-03-07 18:45:47 -0600

    soluccion

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,7 @@
+const { defineConfig } = require('cypress')
 
-import { defineConfig } from 'cypress'
-
-export default defineConfig({
+module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'http://localhost:5174',
   },
 })
